3c3a7f2e677c7b9a3dbbf7b7f9af8fa09963d286
Shamimayesmin <shamimayesmin144@gmail.com>
2023-06-19 22:40:23 +0600

    student service created

---
+++ src/app/modules/student/student.services.ts
@@ -0,0 +1,102 @@
+import { SortOrder } from 'mongoose';
+// import ApiError from '../../../Errors/ApiError';
+import { paginationHelpers } from '../../../helpers/paginationHelpers';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+
+// import httpStatus from 'http-status';
+import { IStudent, IStudentFilters } from './student.interface';
+import { studentSearchableFields } from './student.constant';
+import { Student } from './student.model';
+
+const getAllStudents = async (
+  filters: IStudentFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IStudent[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: studentSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculaterPagination(paginationOptions);
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await Student.find(whereConditions)
+    .populate('academicSemester')
+    .populate('academicDepartment')
+    .populate('academicFaculty')
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Student.countDocuments(whereConditions);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const getSingleStudent = async (id: string): Promise<IStudent | null> => {
+  const result = await Student.findById(id)
+    .populate('academicSemester')
+    .populate('academicDepartment')
+    .populate('academicFaculty');
+  return result;
+};
+
+const updateStudent = async (
+  id: string,
+  payload: Partial<IStudent>
+): Promise<IStudent | null> => {
+  const result = await Student.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteStudent = async (id: string): Promise<IStudent | null> => {
+  const result = await Student.findByIdAndDelete(id)
+    .populate('academicSemester')
+    .populate('academicDepartment')
+    .populate('academicFaculty');
+  return result;
+};
+
+export const StudentService = {
+  getAllStudents,
+  getSingleStudent,
+  updateStudent,
+  deleteStudent,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { UserRouter } from '../modules/user/user.route';
 import { AcademicSemesterRouter } from '../modules/academicSemester/academicSemester.route';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
+import { StudentRouter } from '../modules/student/student.route';
 
 const router = express.Router();
 
@@ -23,10 +24,10 @@ const moduleRoutes = [
     path: '/academic-departments',
     route: academicDepartmentRoutes,
   },
-  // {
-  //   path: '/students',
-  //   route: StudentRoutes,
-  // },
+  {
+    path: '/students',
+    route: StudentRouter,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
